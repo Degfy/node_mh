93a63a23434d51556e255252411c28e5a264446b
king <king@chanpin100.com>
2016-02-05 18:44:15 +0800

    update

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function write_configfile(configData, origin_config, success_fn) {
   utils.extend(origin_config, configData);
   utils.extend(origin_config, options);
   delete origin_config._;
-  
+
   var config_str = 'module.exports = ' + JSON.stringify(origin_config, '', 2) + ';';
   fs.writeFile(config_file, config_str, 'utf8', success_fn);
 }
@@ -65,14 +65,15 @@ function serverStart() {
   var configs = require('./config.js');
   configs.app_path = path.resolve(configs.app_path || '.');
   configs.template = path.resolve(configs.template || './template');
+  configs.cache_path = path.resolve(configs.cache_path || './cache');
   utils.extend(options, configs);
 
 
   var express = require('express'),
     app = express(),
 
-    //课程主页
-    _course = require('./lib/handlers/course.js')(options),
+    // //课程主页
+    // _course = require('./lib/handlers/course.js')(options),
 
     //课程列表页
     _list = require('./lib/handlers/list.js')(options),
@@ -87,8 +88,9 @@ function serverStart() {
     _default = require('./lib/handlers/default.js')(options);
 
   console.log('开始服务了哦:http://' + options.host + (options.port == '80' ? '' : ':' + options.port));
+
   app
-    .use('/course', _course)
+  // .use('/course', _course)
     .use('/list', _list)
     .use('/pay', _pay)
     .use('/apply', _apply)
